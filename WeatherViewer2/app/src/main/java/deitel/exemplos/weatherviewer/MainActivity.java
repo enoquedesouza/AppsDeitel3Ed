3d487a6d7f19875eee07d6b1ba8294042855d5b1
enoquedesouza <enoquedesouza@yahoo.com.br>
2019-01-31 17:47:38 -0200

    Código melhor comentado

--- WeatherViewer2/app/src/main/java/deitel/exemplos/weatherviewer/MainActivity.java
+++ WeatherViewer2/app/src/main/java/deitel/exemplos/weatherviewer/MainActivity.java
@@ -36,41 +36,60 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Weather> weatherList = new ArrayList<>();
+    private List<Weather> weatherList = new ArrayList<>(); //lista de objetos weather
+
+    //Adaptador para o ListView. fornece um Item view personalizado com as iformações climáticas de um dia.
     private WeatherArrayAdapter weatherArrayAdapter;
-    private ArrayAdapter<Weather> array;
+
+    //ListView para exibir as informações  climáticas obtidas do web service
     private ListView weatherListView;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        weatherListView = (ListView) findViewById(R.id.weatherListview);
+        weatherListView = (ListView) findViewById(R.id.weatherListview); // incializa o ListView
+
+        // Define o adaptador com uma ista de objetos Weather
         weatherArrayAdapter = new WeatherArrayAdapter(this, weatherList);
-        array = new ArrayAdapter<Weather>(this, R.layout.list_item);
+
+        //Seta o adaptador do ListView
         weatherListView.setAdapter(weatherArrayAdapter);
 
 
-
+        // Inicializa o floating button (fica suspenso na tela próximo a caixa de texto)
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                // Inicializa a caixa de texto onde o usuário digitará o local do qual ele deseja informações
                 EditText locationEditText = (EditText) findViewById(R.id.locationEditText);
+
+                //Configura um o objeto URL utilizado para se conecatar ao web service
                 URL url = createUrl(locationEditText.getText().toString());
+
                 if(url != null){
 
+                    //Oculta o teclado
                     dismissKeyboard(locationEditText);
+
+                    //Cria uma tarefa assíncrona para a conexão e dowloada das iformações climáticas
                     GetWeatherTask getLocalWeatherTask = new GetWeatherTask();
+
+                    // Executa a terefa
                     getLocalWeatherTask.execute(url);
+
                 }else {
+
+                    // Se ocorrer algum problema na criação do URL
                     Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.invalid_url, Snackbar.LENGTH_LONG)
                             .show();
                 }
@@ -78,24 +97,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // @param view, view de referência do floating button
     private void dismissKeyboard(View view){
 
+        // recupera uma intância de InputMethodManager para acessar o serviço de teclado
         InputMethodManager im = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        //Esconde o teclado
         im.hideSoftInputFromWindow(view.getWindowToken(), 0);
 
     }
 
+    // @param city, nome da cidade fornecida pelo usuário da qual ele deseja as informações climáticas
     private URL createUrl(String city){
 
-        String apiKey = getString(R.string.api_key); // Minha chave de api
-        String baseUrl = getString(R.string.web_service_url);
+
+        // Chave de API necessária para acessar os serviço de forncimento de informações do clima
+        // fornecidos por https://openweathermap.org
+        String apiKey = getString(R.string.api_key);
+
+        String baseUrl = getString(R.string.web_service_url);//endereço base do web service
 
         try{
-            String urlString = baseUrl + URLEncoder.encode(city, "UTF-8") +
-                    "&units=imperial&cnt=15&APPID=" + apiKey; //Minah chave de api não permite carrregar os dados para 16 dias
-                                                            //pois é a versão gratuita, então deixei o padrão 7 dias.
 
+            //Endereço para a requisição do serviço
+            String urlString = baseUrl + URLEncoder.encode(city, "UTF-8") +
+                    "&units=imperial&cnt=15&APPID=" + apiKey;
+
+            // URL para conexão ao web service e requisição do serviço
             return new URL(urlString);
+
+
         }catch (Exception e){
 
             e.printStackTrace();
@@ -105,39 +137,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Se conecta ao web service requisita as iformações sobre o clima para 7 dias e processas as informações recebidas
-    //As informações recebidas vem no formato JSON e precisam ser recuperadas e formatadas para melhor manipulação no software
-    //por isso são passadas para um ArrayList, weatherList.
+    //AsyncTask para a conexão com o web service e download das informações.
     private class GetWeatherTask extends AsyncTask<URL, Void, JSONObject>{
 
+        // Executa a tarefa em plano de fundo
+        //@param <tipo> ... params é um array de tamanho variável onde  tipo depende da tarfa a ser executada e é passado
+        //na chamada de execute
         @Override
         protected JSONObject doInBackground(URL... params) {
 
-            HttpURLConnection connection = null;
+            HttpURLConnection connection = null; // cria um HttpURLConnection para uma conexão HTTP
 
             try{
 
-                connection = (HttpURLConnection) params[0].openConnection();
-                int response = connection.getResponseCode();
 
+                //Abre a conexão com o endereo URL passado como parâmetro
+                connection = (HttpURLConnection) params[0].openConnection();
+
+                int response = connection.getResponseCode(); //Recupera a resposta do servidor
+
+                //Se statur HTTP_OK, ou seja os dados foram enviados
                 if(response == HttpURLConnection.HTTP_OK){
 
-                    StringBuilder builder = new StringBuilder();
+                    StringBuilder builder = new StringBuilder(); //Deine um construtor de strings
 
 
+                    // Cria um Buffer para a leitura dos streams enviado pelo servidor
                     try(BufferedReader reader = new BufferedReader(
+
                            new InputStreamReader(connection.getInputStream()))){
 
                         String line;
 
+                        //Enquanto ouver strings para serem lidas no stream
                         while((line = reader.readLine()) != null){
 
-
-                                builder.append(line);
+                                builder.append(line); //Junte a linha com a linha anterioir para forma a string
 
                         }
 
+                        // Cria um objeto JSON com a string contruida a partir dos dados enviados pelo servidor
                         JSONObject jsonObject = new JSONObject(builder.toString());
+
+                        //Retorna o objeto para ser utilizado na GUI
                         return  jsonObject;
 
                     }catch(Exception e){
@@ -168,37 +210,46 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
+        //@param, object, objeto JSON passado como parâmetro pela AsyncTask
+        //este método realiza suas operações na GUI
         protected void onPostExecute(JSONObject object){
 
 
-            convertJSONtoArrayList(object);
-            weatherArrayAdapter.notifyDataSetChanged();
-            weatherListView.smoothScrollToPosition(0);
+            convertJSONtoArrayList(object); //Adiciona as informações de interesse do objeto JSON em ArrayList
+
+            weatherArrayAdapter.notifyDataSetChanged(); //Notifica o ListView que os dados mudaram e precisa ser atualizado
+
+            weatherListView.smoothScrollToPosition(0); // Rola o ListView para a primeira posição da lista
 
 
 
         }
     }
 
-    //Recupera as informações de um objeto JSON e as armazenas em um arraylist.
+    //@param forecast, objeto JSON passado pela AsyncTask. Nele contém as informações enviada pelo web service
+
     private void convertJSONtoArrayList(JSONObject forecast){
 
-        weatherList.clear();
+        weatherList.clear(); // limpa a lista de weathers
 
 
         try{
 
-            JSONArray list = forecast.getJSONArray("list");
+            JSONArray list = forecast.getJSONArray("list"); //recupera um array de JSONs onde estão as informações de interesse
 
             for(int i = 0; i < list.length(); i++){
 
                 JSONObject day = list.getJSONObject(i);
 
 
+                //Recuepra o objeto JSON no qual estão as informações de temperatura
                 JSONObject temperatures = day.getJSONObject("main");
 
+                //Recuepra o objeto JSON no qual estão a descrição do tempo e icone que representa as condições
+                //climáticas
                 JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
 
+                //cria um objeto weather que armazena as condições climáticas d eum dia.
                 Weather weather1 = new Weather(
 
                         day.getLong("dt"),
@@ -208,6 +259,7 @@ public class MainActivity extends AppCompatActivity {
                         weather.getString("description"),
                         weather.getString("icon"));
 
+                //Adiciona este objeto na lista
                 weatherList.add(weather1);
 
             }
--- WeatherViewer2/app/src/main/java/deitel/exemplos/weatherviewer/WeatherArrayAdapter.java
+++ WeatherViewer2/app/src/main/java/deitel/exemplos/weatherviewer/WeatherArrayAdapter.java
@@ -23,26 +23,22 @@ import java.util.List;
 /**
  * Created by Enoque on 29/01/2019.
  *
- *  A classe extend ArrayAdapter, a idéia é é personalizar um elemento do ListView para que possua um layout
- *  onde seja possível exibir as informações recuperadas do web service. Só que para mexer nos views de um ListView
- *  é preciso personalizar o seu adaptador, que o objeto que exibirá os views com um formato específico, sobreescrevendo
- *  o método getView  chamado pelo ListView quando seu adpatodr é setado.
- *
+ *  Esta class este ArrayAdpater e a personaliza de forma a retorna um view para o ListView
+ *  no qual contém as informações climáticas guardadas por um objeto Weather
  *
  */
 
 public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
 
 
-    //Holder do ArrayAdapter é quem gerencia os elementos view de um widget
-    //é através do holder que os elementos view do arrayadapter são manipulados
+    //Um Hollder para guardar os views que irão compor o item de ListView
     private static class ViewHolder{
 
-        ImageView conditionImageView;
-        TextView dayTextView;
-        TextView lowTextView;
-        TextView hitTextView;
-        TextView humidityTextView;
+        ImageView conditionImageView; // para o ícone representando as condições
+        TextView dayTextView; // para o nome do dia ao se referem as informações
+        TextView lowTextView; // para mínima temperatura no dia
+        TextView hitTextView; // para a máxima temperatura no dia
+        TextView humidityTextView; // para umidade relativa do ar
 
     }
 
@@ -53,16 +49,14 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
     /*
     *
     * @param context, contexto da atividade onde os views serão exibidos
-    * @param forecast, lista de dados Weather (tempo ou clima) que o adaptador será responsável de anexar
-    * ao ListView.
+    * @param forecast, lista de objetos Weather para serem exibidos no ListView
     *
     */
 
-    private Context context;
+
     public WeatherArrayAdapter(Context context, List<Weather> lista){
 
         super(context, -1, lista); //chama o construtor a superclasse
-        this.context = context;
 
     }
 
@@ -74,19 +68,27 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
     public View getView(int position, View convertView, ViewGroup parent){
 
 
-        Weather day = getItem(position);
-        ViewHolder viewHolder;
+        Weather day = getItem(position);// Instância do objeto Weather na posição do item atual.
+
+        ViewHolder viewHolder; // para o holder dos elementos do item
 
         if(convertView == null){
 
             viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
+
+            LayoutInflater inflater = LayoutInflater.from(getContext()); // Inflador do layout do view
+
+            //Infla o layout personalizado para o item de ListView definido list_item
             convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+            //Incializa os elementos do view
             viewHolder.conditionImageView = (ImageView) convertView.findViewById(R.id.conditionImageView);
             viewHolder.dayTextView = (TextView) convertView.findViewById(R.id.dayTextView);
             viewHolder.lowTextView = (TextView) convertView.findViewById(R.id.lowTextView);
             viewHolder.hitTextView = (TextView) convertView.findViewById(R.id.hitTextView);
             viewHolder.humidityTextView = (TextView) convertView.findViewById(R.id.humidityTextView);
+
+            // Agrupa o elementos no view a ser retornado
             convertView.setTag(viewHolder);
 
         }else{
@@ -105,6 +107,7 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
 
         Context context = getContext();
 
+        // Seta os elementos com os valores do objeto Weather presente na lista
         viewHolder.dayTextView.setText(context.getString(R.string.day_description,
                                         day.dayOfWeek, day.description));
 
@@ -115,13 +118,14 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
         viewHolder.humidityTextView.setText(context.getString(R.string.humidity, day.humidity));
 
 
+        // Retorna o view personalizado para o ListView o exibir
         return convertView;
 
     }
 
-    //Tarefa excutado a parte para baixar a imagem que representa as condições climática
-    // a tarefa é exectada em uma thread separada e depois o resultado do procssamento é enviado
-    //para a GUI Thread para que a interface gráfica seja atualizada.
+    // AsyncTask para o dowload da imagem que representa as condições climáticas do dia
+    // a imagem não baixada na conexão, pois o padrão de retorno é JSON, ou seja, tipo texto
+    // por isso deve ser baixada a parte.
     private class LoadImageTask extends AsyncTask<String, Void, Bitmap>{
 
         private  ImageView loadImageView;
